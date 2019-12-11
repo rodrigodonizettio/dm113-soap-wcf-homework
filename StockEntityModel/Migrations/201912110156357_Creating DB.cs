570332c7cee2252b96c74c76c08e3965cb8dd47a
rodrigodonizettio <rodrigodonizettio@gmail.com>
2019-12-10 23:14:10 -0300

    Feature: Creating StockEntityModel DB and Populating with some ProductStock inside
    The products were inserted using the Seed method at Configuration.cs

---
+++ StockEntityModel/Migrations/201912110156357_Creating DB.cs
@@ -0,0 +1,29 @@
+﻿namespace StockEntityModel.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreatingDB : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ProductStocks",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Number = c.String(nullable: false),
+                        Name = c.String(nullable: false),
+                        Description = c.String(),
+                        Amount = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.ProductStocks");
+        }
+    }
+}
--- StockEntityModel/Migrations/Configuration.cs
+++ StockEntityModel/Migrations/Configuration.cs
@@ -14,10 +14,21 @@
 
         protected override void Seed(StockEntityModel.StockProvider context)
         {
-            //  This method will be called after migrating to the latest version.
+            context.ProductsStock.AddOrUpdate(
+                p => p.Id,
+                new ProductStock { Number = "1000", Name = "Product 1", Description = "This is the Product 1", Amount = 100 },
+                new ProductStock { Number = "2000", Name = "Product 2", Description = "This is the Product 2", Amount = 10 },
+                new ProductStock { Number = "3000", Name = "Product 3", Description = "This is the Product 3", Amount = 200 },
+                new ProductStock { Number = "4000", Name = "Product 4", Description = "This is the Product 4", Amount = 300 },
+                new ProductStock { Number = "5000", Name = "Product 5", Description = "This is the Product 5", Amount = 400 },
+                new ProductStock { Number = "6000", Name = "Product 6", Description = "This is the Product 6", Amount = 500 },
+                new ProductStock { Number = "7000", Name = "Product 7", Description = "This is the Product 7", Amount = 30 },
+                new ProductStock { Number = "8000", Name = "Product 8", Description = "This is the Product 8", Amount = 30 },
+                new ProductStock { Number = "9000", Name = "Product 9", Description = "This is the Product 9", Amount = 400 },
+                new ProductStock { Number = "10000", Name = "Product 10", Description = "This is the Product 10", Amount = 2 }
+            );
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
+            context.SaveChanges();
         }
     }
 }

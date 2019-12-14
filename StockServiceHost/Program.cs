f3b022dd9d314a23cf99b4362141d03e6d8954f6
rodrigodonizettio <rodrigodonizettio@gmail.com>
2019-12-14 14:51:04 -0300

    Feature: Creating and Implementing StockServiceHost project

---
+++ StockServiceHost/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel;
+using  StockService;
+
+namespace StockServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost stockServiceHost = new ServiceHost(typeof(StockService.StockService));
+            stockServiceHost.Open();
+            Console.WriteLine("Service is running");
+            Console.WriteLine("Type any key to stop it");
+
+            Console.ReadLine();
+            Console.WriteLine("Service is being stopped");
+            stockServiceHost.Close();
+        }
+    }
+}

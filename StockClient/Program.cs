a895640b7e170bbb2cc47d801136b10578c0b5c7
rodrigodonizettio <rodrigodonizettio@gmail.com>
2019-12-17 19:35:41 -0300

    Feature: Adding methods to the StockClient project to test CRUD methods from ProductStockService project

--- StockClient/Program.cs
+++ StockClient/Program.cs
@@ -14,21 +14,120 @@ namespace StockClient
     {
         static void Main(string[] args)
         {
-            // Create a proxy object and connect to the service
-            StockService.StockServiceClient proxy = new StockService.StockServiceClient();
+            StockService.StockServiceClient proxy = OpenProxyConnection();
 
-            // Test the operations in the service
-            // Obtain a list of all products
-            Console.WriteLine("Test 1: List all products");
+            ShouldCreateProductStock(proxy);            
+            ShouldRetrieveAllProductsStock(proxy);
+            ShouldRetrieveProductStock(proxy);
+            ShouldUpdateProductStockAmount(proxy);
+            ShouldRetrieveProductStockAmount(proxy);
+            shouldDeleteProductStock(proxy);
+            
+            CloseProxyConnection(proxy);
+        }
+
+
+        public static void ShouldCreateProductStock(StockService.StockServiceClient proxy)
+        {
+            Console.WriteLine("Test - CREATE a ProductStock");
+            StockService.ProductStock productStock = new StockService.ProductStock();
+            productStock.Number = "number1";
+            productStock.Name = "name1";
+            productStock.Description = "description1";
+            productStock.Amount = 1;
+
+            if(proxy.CreateProductStock(productStock))
+            {
+                Console.WriteLine("ProductStock Name: " + productStock.Name + " was created successfully");
+            }
+            else
+            {
+                Console.WriteLine("Failed to create a new ProductStock");
+            }
+            Console.WriteLine();
+        }
+
+        public static void shouldDeleteProductStock(StockService.StockServiceClient proxy)
+        {
+            Console.WriteLine("Test - DELETE a ProductStock");
+            string productStockNumber = "number1";
+            if(proxy.DeleteProductStock(productStockNumber))
+            {
+                Console.WriteLine("ProductStock Number: " + productStockNumber + " was deleted successfully");
+            }
+            else
+            {
+                Console.WriteLine("Failed to delete a new ProductStock");
+            }
+            Console.WriteLine();
+        }
+
+        public static void ShouldRetrieveAllProductsStock(StockService.StockServiceClient proxy)
+        {
+            Console.WriteLine("Test - RETRIEVE ALL ProductsStock");
             List<string> productStockDataNames = proxy.RetrieveAllProductsStock().ToList();
             foreach (string name in productStockDataNames)
             {
-                Console.WriteLine("Name: {0}", name);
-                Console.WriteLine();
+                Console.WriteLine("ProductStock Name: {0}", name);
+            }
+            Console.WriteLine("ProductsStock were retrieved successfully");
+            Console.WriteLine();
+        }
+
+        public static void ShouldRetrieveProductStock(StockService.StockServiceClient proxy)
+        {
+            Console.WriteLine("Test - RETRIEVE ProductStock BY NUMBER");
+            string productStockNumber = "number1";
+
+            StockService.ProductStockData productStockData = proxy.RetrieveProductStock(productStockNumber);
+
+            if (productStockData != null)
+            {
+                Console.WriteLine("ProductStock - Number: " + productStockData.Number);
+                Console.WriteLine("ProductStock - Name: " + productStockData.Name);
+                Console.WriteLine("ProductStock - Description: " + productStockData.Description);
+                Console.WriteLine("ProductStock - Amount: " + productStockData.Amount);
+                Console.WriteLine("ProductStock Number: " + productStockNumber + " was retrieved successfully");
+            }
+            else
+            {
+                Console.WriteLine("Failed to retrieve the ProductStock Number: " + productStockNumber);
             }
             Console.WriteLine();
+        }
 
-            // Disconnect from the service
+        public static void ShouldUpdateProductStockAmount(StockService.StockServiceClient proxy)
+        {
+            Console.WriteLine("Test - UPDATE a ProductStock AMOUNT");
+            string productStockNumber = "number1";
+            int newProductStockAmount = 1;
+            if(proxy.UpdateProductStockAmount(productStockNumber, newProductStockAmount))
+            {
+                Console.WriteLine("ProductStock Number: " + productStockNumber + " had its amount updated successfully");
+            }
+            else
+            {
+                Console.WriteLine("Failed to update the amount of the ProductStock Number: " + productStockNumber);
+            }
+            Console.WriteLine();
+        }
+
+        public static void ShouldRetrieveProductStockAmount(StockService.StockServiceClient proxy)
+        {
+            Console.WriteLine("Test - RETRIEVE a ProductStock AMOUNT");
+            string productStockNumber = "number1";
+            int productStockAmount = proxy.RetrieveProductStockAmount(productStockNumber);
+            Console.WriteLine("ProductStock amount is: " + productStockAmount);
+            Console.WriteLine();
+        }
+
+        public static StockService.StockServiceClient OpenProxyConnection()
+        {
+            return new StockService.StockServiceClient();
+        }
+
+        public static void CloseProxyConnection(StockService.StockServiceClient proxy)
+        {
             proxy.Close();
             Console.WriteLine("Press ENTER to finish");
             Console.ReadLine();

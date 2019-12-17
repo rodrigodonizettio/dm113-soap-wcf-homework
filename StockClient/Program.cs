fc9ba86541cc488411d62d7034b62d96a5b2a886
rodrigodonizettio <rodrigodonizettio@gmail.com>
2019-12-17 00:21:01 -0300

    Feature: Implementing Client service to call RetrieveAllProductsStock method

---
+++ StockClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel;
+using StockClient.StockService;
+using StockClient.StockServiceV2;
+
+namespace StockClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a proxy object and connect to the service
+            StockService.StockServiceClient proxy = new StockService.StockServiceClient();
+
+            // Test the operations in the service
+            // Obtain a list of all products
+            Console.WriteLine("Test 1: List all products");
+            List<string> productStockDataNames = proxy.RetrieveAllProductsStock().ToList();
+            foreach (string name in productStockDataNames)
+            {
+                Console.WriteLine("Name: {0}", name);
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            // Disconnect from the service
+            proxy.Close();
+            Console.WriteLine("Press ENTER to finish");
+            Console.ReadLine();
+        }
+    }
+}
